47afddbc28e9d092bd469bac0024a27c5673d312
Braden CT Tan <tct13@yahoo.com>
2016-11-08 13:40:37 +0800

    latetst working

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :users, path: '', path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'register'
+  }
+
+  resources :ideas
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "ideas#index"
+end
